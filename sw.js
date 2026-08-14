47cbb74ea03c7b67c050b1cffb49397fc6423a4b
Marks2099 <marek.gajdusek@gmail.com>
2026-08-15 01:00:24 +0200

    Aktualizace offline cache pro logo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="vestope-groomer-phase-a-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
+const CACHE="vestope-groomer-phase-a-v2";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./logo.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
 self.addEventListener("fetch",e=>{const u=new URL(e.request.url);if(u.origin===location.origin)e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match("./index.html"))))});
