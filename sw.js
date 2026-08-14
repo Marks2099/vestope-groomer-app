9ce9819e586c3a7eff5879b9ff91ace222f23d49
Marks2099 <marek.gajdusek@gmail.com>
2026-08-15 01:06:02 +0200

    Offline cache používá originální PNG logo VeStope.cz

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE="vestope-groomer-phase-a-v2";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./logo.svg"];
+const ASSETS=["./","./index.html","./manifest.webmanifest","./logo_vestope.cz.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{const u=new URL(e.request.url);if(u.origin===location.origin)e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match("./index.html"))))});
